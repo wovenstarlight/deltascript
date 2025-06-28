713fd8e219335c31710fb9b366d4119c04fdf5ce
Devishi Ranjan <drconker@gmail.com>
2025-06-29 03:32:31 +0530

    Add face sprites
    - Alphys
    - Berdly (Light/Dark worlds)
    - Bratty & Catty
    - Burgerpants
    - Carol
    - Catti
    - Jockington('s single sprite...)
    - King
    - Rouxls (+ Pirate hat version)
    - Sans
    - Susie's hidden eye version
    - Undyne

--- js/global.js
+++ js/global.js
@@ -79,6 +79,7 @@ class DialogueSprite extends HTMLElement {
 			}${emotion ? `/${emotionToIndex(emotion)}` : ""
 			}.${
 				(speaker === "Toriel" && [0, 1, 2, 6, 7, 9].includes(emotionToIndex(emotion)))
+				|| (speaker === "Susie" && emotionToIndex(emotion) === 11)
 				? "gif"	// `gif` in certain cases
 				: "png"	// `png` otherwise
 			}"
